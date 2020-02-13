d8b705c2bc2028c371986886841fab8c7f83e6f2
Ian Norton <ian.norton@matillion.com>
2020-02-13 22:18:30 +0000

    Refactoring volunteer admin display
    * Enable modal dialogs for edit and view of records
    * Limit fields displayed in volunteer list
    * Increase displayed records to 50 and allow change in UI
    * Change ordering of category and imports of views to structure menu bar
    * Exclude version field from forms
    * Add script-src for edit and view dialogs to permit csp to work

--- apps/volunteer/admin/volunteer.py
+++ apps/volunteer/admin/volunteer.py
@@ -5,10 +5,31 @@ from models.volunteer.volunteer import Volunteer
 
 
 class VolunteerUserModelView(VolunteerModelView):
-    column_searchable_list = ("nickname", "volunteer_email")
+    can_delete = False
+    action_disallowed_list = ['delete']
+    can_set_page_size = True
+    can_view_details = True
     column_filters = ["trained_roles", "allow_comms_during_event"]
+    column_list = (
+        "user",
+        "planned_arrival",
+        "planned_departure",
+        "nickname",
+        "banned",
+    )
+    column_searchable_list = ("nickname", "volunteer_email")
+    create_modal = True
+    details_modal = True
+    edit_modal = True
+    form_excluded_columns = ("versions")
+    page_size = 50  # the number of entries to display on the list view
 
 
+# Add menu item Volunteers
 volunteer_admin.add_view(
-    VolunteerUserModelView(Volunteer, db.session, category="volunteers")
+    VolunteerUserModelView(
+        Volunteer,
+        db.session,
+        name="Volunteers"
+    )
 )
