92a40a3d0e4be6a4a8a55d6acb3df27dc9d177ca
Ian Norton <ian.norton@matillion.com>
2020-02-14 21:12:09 +0000

    Reorder volunteer admin form fields to flow better

--- apps/volunteer/admin/volunteer.py
+++ apps/volunteer/admin/volunteer.py
@@ -5,23 +5,49 @@ from models.volunteer.volunteer import Volunteer
 
 
 class VolunteerUserModelView(VolunteerModelView):
+    can_create = False
     can_delete = False
-    action_disallowed_list = ['delete']
     can_set_page_size = True
     can_view_details = True
-    column_filters = ["trained_roles", "allow_comms_during_event"]
-    column_list = (
-        "user",
+    column_details_exclude_list = ("user", "versions")
+    column_details_list = (
+        "nickname",
+        "volunteer_email",
+        "volunteer_phone",
         "planned_arrival",
         "planned_departure",
+        "missing_shifts_opt_in",
+        "over_18",
+        "allow_comms_during_event",
+        "banned",
+    )
+    column_filters = ["trained_roles", "allow_comms_during_event"]
+    column_list = (
         "nickname",
+        "volunteer_email",
+        "planned_arrival",
+        "planned_departure",
         "banned",
     )
     column_searchable_list = ("nickname", "volunteer_email")
-    create_modal = True
     details_modal = True
     edit_modal = True
-    form_excluded_columns = ("versions")
+    form_columns = (
+        "nickname",
+        "volunteer_email",
+        "volunteer_phone",
+        "planned_arrival",
+        "planned_departure",
+
+        "interested_roles",
+        "trained_roles",
+
+        "missing_shifts_opt_in",
+        "over_18",
+        "allow_comms_during_event",
+        "banned",
+    )
+    form_excluded_columns = ("user", "versions")
     page_size = 50  # the number of entries to display on the list view
 
 
